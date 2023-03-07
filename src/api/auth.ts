464608d01df8adf8db1e5170aadf84db094cc9cc
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2023-03-07 22:30:43 +0700

    create user in Firestore

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,25 @@
 import {RegisterForm} from "../types/Form";
 import {createUserWithEmailAndPassword} from "@firebase/auth";
-import {firebaseAuth} from "../db";
+import {db, firebaseAuth} from "../db";
+import {User} from "../types/User";
+import {setDoc, doc} from "@firebase/firestore";
 
-const registerUser = (form: RegisterForm) => {
-    return createUserWithEmailAndPassword(firebaseAuth, form.email, form.password);
+const registerUser = async (form: RegisterForm) => {
+    const {user: registeredUser} = await createUserWithEmailAndPassword(firebaseAuth, form.email, form.password);
+    const user: User = {
+        uid: registeredUser.uid,
+        fullName: form.fullName,
+        nickName: form.nickName,
+        email: form.email,
+        avatar: form.avatar,
+        followers: [],
+        following: [],
+        followersCount: 0,
+        followingCount: 0,
+    }
+
+    await setDoc(doc(db, "users", registeredUser.uid), user);
+    return registeredUser;
 }
 
 export {registerUser};
