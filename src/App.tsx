55c3fdd07ec3b4b20ac812d4c58871dd3070b555
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2023-03-08 20:02:40 +0700

    add snackbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {Component} from "solid-js";
 import AppRoutes from "./router";
 import {useAuthState} from "@context/auth";
+import SnackbarContainer from "@components/snackbar/Container";
 
 const App: Component = () => {
     const authState = useAuthState()!;
     return (
         <>
             <div id="popups"/>
+            <SnackbarContainer/>
             <AppRoutes/>
         </>
     )
