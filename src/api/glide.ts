ba115dbc95a12920d80a49de80985344ae75a048
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2023-03-10 22:23:31 +0700

    use Glides hook to fetch Glides from Firestore

--- src/api/glide.ts
+++ src/api/glide.ts
@@ -1,9 +1,37 @@
-import {addDoc, collection, doc, Timestamp} from "@firebase/firestore";
+import {
+    addDoc,
+    collection,
+    doc,
+    DocumentReference,
+    getDoc,
+    getDocs, limit,
+    orderBy,
+    query,
+    Timestamp
+} from "@firebase/firestore";
 import {db} from "../db";
 import {Glide} from "../types/Glide";
+import {User} from "../types/User";
 
-const getGlides = () => {
-    alert("Getting new glides!");
+const getGlides = async () => {
+    const constraints = [
+        orderBy("date", "desc"),
+        limit(10),
+    ]
+    const q = query(collection(db, "glides"), ...constraints);
+    const qSnapshot = await getDocs(q);
+
+    const glides = await Promise.all(qSnapshot.docs.map(async doc => {
+        const glide = doc.data() as Glide;
+        const userSnap = await getDoc(glide.user as DocumentReference);
+        glide.user = userSnap.data() as User;
+        return {
+            ...glide,
+            id: doc.id,
+        }
+    }));
+
+    return {glides};
 }
 
 const createGlide = async (form: {
--- src/hooks/useGlides.ts
+++ src/hooks/useGlides.ts
@@ -1,27 +1,49 @@
 import {Glide} from "../types/Glide";
-import {createStore} from "solid-js/store";
-import {onMount} from "solid-js";
+import {createStore, produce} from "solid-js/store";
+import {createSignal, onMount} from "solid-js";
 import {getGlides} from "../api/glide";
+import {FirebaseError} from "@firebase/app";
 
 type State = {
-    glides: Glide[];
+    pages: {
+        [key: string]: { glides: Glide[] }
+    };
     loading: boolean;
 }
 
-const createInitState = () => ({glides: [], loading: false});
+const createInitState = () => ({pages: {}, loading: false});
 
 const useGlides = () => {
+    const [page, setPage] = createSignal(1);
     const [store, setStore] = createStore<State>(createInitState());
 
     onMount(() => {
         loadGlides();
     })
 
-    const loadGlides = () => {
-        getGlides();
+    const loadGlides = async () => {
+        const _page = page();
+        setStore("loading", true);
+        try {
+            const {glides} = await getGlides();
+
+            if (glides.length > 0) {
+                setStore(produce(store => {
+                    store.pages[_page] = {
+                        glides
+                    }
+                }));
+            }
+        } catch (error) {
+            const message = (error as FirebaseError).message;
+            console.log(message);
+        } finally {
+            setStore("loading", false);
+        }
     }
 
     return {
+        page,
         loadGlides,
         store,
     }
