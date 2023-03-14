b3939089c89833f4edce23f52954115320c3159b
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2023-03-14 15:35:42 +0700

    Back Button

--- src/screens/GlideDetail.tsx
+++ src/screens/GlideDetail.tsx
@@ -4,13 +4,21 @@ import {createResource, onMount, Show} from "solid-js";
 import {getGlideById} from "../api/glide";
 import GlidePost from "@components/glides/GlidePost";
 import {CenteredDataLoader} from "@components/utils/DataLoader";
+import {FaSolidArrowLeft} from "solid-icons/fa";
 
 const GlideDetailScreen = () => {
     const params = useParams();
     const [data] = createResource(() => getGlideById(params.id, params.uid));
 
     return (
-        <MainLayout pageTitle="Detail">
+        <MainLayout pageTitle={
+            <div onClick={() => history.back()}>
+                <div class="flex-it flex-row items-center text-xl cursor-pointer">
+                    <FaSolidArrowLeft/>
+                    <div class="ml-5 font-bold">Back</div>
+                </div>
+            </div>
+        }>
             <Show
                 when={!data.loading}
                 fallback={<CenteredDataLoader/>}
