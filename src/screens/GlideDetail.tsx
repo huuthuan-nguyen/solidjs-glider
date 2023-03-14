ff9784b6612f2e519103efe1fe5ba9e2467615e4
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2023-03-14 14:47:07 +0700

    get routing params

--- src/screens/GlideDetail.tsx
+++ src/screens/GlideDetail.tsx
@@ -1,9 +1,16 @@
 import MainLayout from "@components/layouts/Main";
+import {useParams} from "@solidjs/router";
 
 const GlideDetailScreen = () => {
+    const params = useParams();
     return (
         <MainLayout pageTitle="Detail">
-            Hello Glide Detail Screen!
+            <div>
+                id: {params.id}
+            </div>
+            <div>
+                uid: {params.uid}
+            </div>
         </MainLayout>
     )
 }
