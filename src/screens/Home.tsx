0a7527a3c5bf51e1be0d45711a42ad5ec3257089
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2023-03-14 12:29:28 +0700

    wrapper button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,6 +4,7 @@ import Messenger from "@components/utils/Messenger";
 import useGlides from "../hooks/useGlides";
 import PaginatedGlides from "@components/glides/PaginatedGlides";
 import {Portal} from "solid-js/web";
+import Button from "@components/utils/Button";
 
 const HomeScreen: Component = () => {
     const {
@@ -27,22 +28,15 @@ const HomeScreen: Component = () => {
         <MainLayout pageTitle="Home">
             <Messenger onGlideAdded={addGlide}/>
             <div class="h-px bg-gray-700 my-1"/>
-            <Show when={store.freshGlides.length >= 3}>
-                <Portal>
-                    <div class="fixed top-2 z-100 left-2/4 -translate-x-1/2">
-                        <button
-                            type="button"
-                            class="
-              disabled:cursor-not-allowed disabled:bg-gray-400
-              bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200"
-                        >
-                            <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
-                                <span>Read New Glides</span>
-                            </div>
-                        </button>
-                    </div>
-                </Portal>
-            </Show>
+            {/*<Show when={store.freshGlides.length >= 3}>*/}
+            <Portal>
+                <div class="fixed top-2 z-100 left-2/4 -translate-x-1/2">
+                    <Button onClick={() => alert("Hi there!")}>
+                        <span>Read New Glides</span>
+                    </Button>
+                </div>
+            </Portal>
+            {/*</Show>*/}
             <PaginatedGlides
                 page={page}
                 pages={store.pages}
