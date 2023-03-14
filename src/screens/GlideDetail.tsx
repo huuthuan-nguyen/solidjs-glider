ac8021ced3d483981750a76ff3db66d188685a36
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2023-03-14 15:20:01 +0700

    display glide with createResource

--- src/screens/GlideDetail.tsx
+++ src/screens/GlideDetail.tsx
@@ -1,24 +1,22 @@
 import MainLayout from "@components/layouts/Main";
 import {useParams} from "@solidjs/router";
-import {onMount} from "solid-js";
+import {createResource, onMount, Show} from "solid-js";
 import {getGlideById} from "../api/glide";
+import GlidePost from "@components/glides/GlidePost";
+import {CenteredDataLoader} from "@components/utils/DataLoader";
 
 const GlideDetailScreen = () => {
     const params = useParams();
-
-    onMount(async () => {
-        const glide = await getGlideById(params.id, params.uid);
-        console.log(glide);
-    })
+    const [data] = createResource(() => getGlideById(params.id, params.uid));
 
     return (
         <MainLayout pageTitle="Detail">
-            <div>
-                id: {params.id}
-            </div>
-            <div>
-                uid: {params.uid}
-            </div>
+            <Show
+                when={!data.loading}
+                fallback={<CenteredDataLoader/>}
+            >
+                <GlidePost glide={data()!}/>
+            </Show>
         </MainLayout>
     )
 }
