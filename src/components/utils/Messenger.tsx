6d04fab3e5a6d638178fc4356d6cffb760805f64
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2023-03-16 02:51:29 +0700

    set image to signal

--- src/components/utils/Messenger.tsx
+++ src/components/utils/Messenger.tsx
@@ -13,7 +13,7 @@ type Props = {
 }
 const Messenger: Component<Props> = (initialProps) => {
     const props = mergeProps({showAvatar: true}, initialProps);
-    const {handleInput, handleSubmit, form, loading} = useMessenger(props.answerTo);
+    const {handleInput, handleSubmit, form, loading, image, setImage} = useMessenger(props.answerTo);
     const {user} = useAuthState()!;
 
     const sendDisabled = () => loading() || form.content.trim() === "";
@@ -37,8 +37,12 @@ const Messenger: Component<Props> = (initialProps) => {
             const blob = new Blob([buffer8Uint], {type: file.type});
             const urlCreator = window.URL || window.webkitURL;
 
-            const imageUrl = urlCreator.createObjectURL(blob);
-            console.log(imageUrl);
+            const previewUrl = urlCreator.createObjectURL(blob);
+            setImage({
+                buffer,
+                name: file.name,
+                previewUrl,
+            });
         }
     }
 
@@ -69,6 +73,11 @@ const Messenger: Component<Props> = (initialProps) => {
               placeholder={"What's new?"}
           />
                 </div>
+                <Show when={image().previewUrl.length > 0}>
+                    <div class="flex-it max-w-52 p-4">
+                        <img src={image().previewUrl} />
+                    </div>
+                </Show>
                 <div class="flex-it mb-1 flex-row xs:justify-between items-center">
                     <div class="flex-it mt-3 mr-3 cursor-pointer text-white hover:text-blue-400 transition">
                         <div class="upload-btn-wrapper">
--- src/hooks/useMessenger.ts
+++ src/hooks/useMessenger.ts
@@ -1,61 +1,78 @@
-import { createStore } from "solid-js/store";
-import { GliderInputEvent, MessengerForm } from "../types/Form";
-import { useAuthState } from "@context/auth";
-import { useUIDispatch } from "@context/ui";
-import { createSignal } from "solid-js";
-import { createGlide } from "../api/glide";
-import { FirebaseError } from "@firebase/app";
+import {createStore} from "solid-js/store";
+import {GliderInputEvent, MessengerForm} from "../types/Form";
+import {useAuthState} from "@context/auth";
+import {useUIDispatch} from "@context/ui";
+import {createSignal} from "solid-js";
+import {createGlide} from "../api/glide";
+import {FirebaseError} from "@firebase/app";
+import * as buffer from "buffer";
+
+
+type UploadImage = {
+    buffer: ArrayBuffer,
+    name: string;
+    previewUrl: string;
+}
+
+const defaultImage = () => ({
+    buffer: new ArrayBuffer(0),
+    name: "",
+    previewUrl: "",
+})
 
 const useMessenger = (answerTo?: string) => {
-  const { isAuthenticated, user } = useAuthState()!;
-  const { addSnackbar } = useUIDispatch();
-  const [loading, setLoading] = createSignal(false);
-  const [form, setForm] = createStore<MessengerForm>({
-    content: "",
-  });
+    const {isAuthenticated, user} = useAuthState()!;
+    const {addSnackbar} = useUIDispatch();
+    const [image, setImage] = createSignal<UploadImage>(defaultImage());
+    const [loading, setLoading] = createSignal(false);
+    const [form, setForm] = createStore<MessengerForm>({
+        content: "",
+    });
 
-  const handleInput = (e: GliderInputEvent) => {
-    const { name, value } = e.currentTarget;
-    setForm(name, value);
-  }
-
-  const handleSubmit = async () => {
-
-    if (!isAuthenticated) {
-      addSnackbar({ message: "You are not authenticated!", type: "error" });
-      return;
+    const handleInput = (e: GliderInputEvent) => {
+        const {name, value} = e.currentTarget;
+        setForm(name, value);
     }
 
-    setLoading(true);
+    const handleSubmit = async () => {
 
-    const glide = {
-      ...form,
-      uid: user!.uid,
+        if (!isAuthenticated) {
+            addSnackbar({message: "You are not authenticated!", type: "error"});
+            return;
+        }
+
+        setLoading(true);
+
+        const glide = {
+            ...form,
+            uid: user!.uid,
+        }
+
+        try {
+            const newGlide = await createGlide(glide, answerTo);
+            newGlide.user = {
+                nickName: user!.nickName,
+                avatar: user!.avatar
+            };
+            addSnackbar({message: "Glide added!", type: "success"});
+            setForm({content: ""});
+            return newGlide;
+        } catch (error) {
+            const message = (error as FirebaseError).message;
+            addSnackbar({message, type: "error"});
+        } finally {
+            setLoading(false);
+        }
     }
 
-    try {
-      const newGlide = await createGlide(glide, answerTo);
-      newGlide.user = {
-        nickName: user!.nickName,
-        avatar: user!.avatar
-      };
-      addSnackbar({ message: "Glide added!", type: "success" });
-      setForm({ content: "" });
-      return newGlide;
-    } catch (error) {
-      const message = (error as FirebaseError).message;
-      addSnackbar({ message, type: "error" });
-    } finally {
-      setLoading(false);
+    return {
+        handleInput,
+        handleSubmit,
+        form,
+        loading,
+        image,
+        setImage,
     }
-  }
-
-  return {
-    handleInput,
-    handleSubmit,
-    form,
-    loading,
-  }
 }
 
 export default useMessenger;
